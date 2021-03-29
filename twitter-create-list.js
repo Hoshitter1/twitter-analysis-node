ad2a8304f93e6f25f21533c2cc36b00dc87ffe1b
Hoshitter1 <hoshi.to.318@gmail.com>
2021-03-29 12:18:50 +0900

    closed #1

--- twitter-create-list.js
+++ twitter-create-list.js
@@ -1,18 +1,12 @@
-// const fs = require("fs");
+const fs = require("fs");
 
-// const filename = "accounts.txt";
-// const names = [];
+const filename = "accounts.txt";
 
-// fs.readFile(filename, "utf8", function (err, data) {
-//   if (err) throw err;
-//   console.log(data);
-//   names.push(data);
-//   console.log("INSIDE FUNC");
-//   console.log(names);
-// });
+fs.readFile(filename, "utf8", function (err, data) {
+  if (err) throw err;
+  console.log("INSIDE DATA");
+  console.log(data.split("\n"));
+});
 
-// console.log("THIS");
-// console.log(names);
-
-const randomText = "hoshito1zilya1takato1icecoffe";
-console.log(randomText.split("1"));
+// const randomText = "hoshito1zilya1takato1icecoffe";
+// console.log(randomText.split("1"));
