bdb62d1dfd7dbcd2ce1f536acf1d89c793b498ad
Hoshitter1 <hoshi.to.318@gmail.com>
2021-03-29 13:32:44 +0900

    renamed js file

--- readTextFile.js
+++ readTextFile.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 
 const filename = "accounts.txt";
-const util = require("util");
 
 const data = fs.readFileSync(filename, "utf-8");
 console.log(data.split("\n"));
 
 // if you want to see if there is new line
+// const util = require("util");
 // const str = util.inspect(data);
 // console.log(str)
