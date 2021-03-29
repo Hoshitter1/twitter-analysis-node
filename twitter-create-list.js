d72f2fd1e7ea90f76d8af47a4ec8dd684fc6fbd0
Hoshitter1 <hoshi.to.318@gmail.com>
2021-03-29 12:33:31 +0900

    show string with new line in it

--- twitter-create-list.js
+++ twitter-create-list.js
@@ -1,12 +1,14 @@
 const fs = require("fs");
 
 const filename = "accounts.txt";
+const util = require("util");
 
 fs.readFile(filename, "utf8", function (err, data) {
   if (err) throw err;
   console.log("INSIDE DATA");
-  console.log(data.split("\n"));
+  const str = util.inspect(data);
+  console.log(str);
+  //   console.log(data.split("\n"));
 });
 
-// const randomText = "hoshito1zilya1takato1icecoffe";
-// console.log(randomText.split("1"));
+// console.log("hello");
