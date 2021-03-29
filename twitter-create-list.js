bc0ea086b4e9bcb049287f185dd23f56016a88bb
Hoshitter1 <hoshi.to.318@gmail.com>
2021-03-29 12:13:35 +0900

    added file manipulation code plus split

---
+++ twitter-create-list.js
@@ -0,0 +1,18 @@
+// const fs = require("fs");
+
+// const filename = "accounts.txt";
+// const names = [];
+
+// fs.readFile(filename, "utf8", function (err, data) {
+//   if (err) throw err;
+//   console.log(data);
+//   names.push(data);
+//   console.log("INSIDE FUNC");
+//   console.log(names);
+// });
+
+// console.log("THIS");
+// console.log(names);
+
+const randomText = "hoshito1zilya1takato1icecoffe";
+console.log(randomText.split("1"));
