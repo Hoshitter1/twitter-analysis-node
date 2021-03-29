2e308dc72e93764903f5bba17b3f289724dd9560
Hoshitter1 <hoshi.to.318@gmail.com>
2021-03-29 15:52:32 +0900

    added the function to calculate intersection

--- calc-set.js
+++ calc-set.js
@@ -1,12 +1,12 @@
 // let intersection = arrA.filter(x => arrB.includes(x));
-const arry1 = [1, 2, 3, 4];
+// const arry1 = [1, 2, 3, 4];
 
-function isNotThree(value) {
-  return value != 3;
-}
+// function isNotThree(value) {
+//   return value != 3;
+// }
 
-const filteredArry = arry1.filter(isNotThree);
-console.log(filteredArry);
+// const filteredArry = arry1.filter(isNotThree);
+// console.log(filteredArry);
 
 /// normal function style
 
@@ -19,3 +19,26 @@ console.log(filteredArry);
 
 // const retVal = isBiggerThanThree(value3);
 // console.log(retVal);
+
+// const arryA = [1, 2, 3, 4];
+// const arryC = arryA.filter((value) => 1 === 1);
+// console.log(arryC);
+
+// const arryA = [1, 2, 3, 4];
+// let arryC = [];
+// for (let i = 0; i < arryA.length; i++) {
+//   const targetValue = arryA[i];
+//   if (targetValue != 3) {
+//     arryC.push(targetValue);
+//   }
+// }
+
+// console.log(arryC);
+
+const arryA = ["abc", "def", "pq"];
+const arryB = ["abc", "aaa", "ddd"];
+function hasValueInArryb(value) {
+  return arryB.includes(value);
+}
+let intersection = arryA.filter(hasValueInArryb);
+console.log(intersection);
