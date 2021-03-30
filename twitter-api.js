f1b9d88bedcb1e8e87a07b528f28ce83f469d36d
Hoshitter1 <hoshi.to.318@gmail.com>
2021-03-30 10:50:46 +0900

    added .env

--- twitter-api.js
+++ twitter-api.js
@@ -1,14 +1,18 @@
 var Twitter = require("twitter");
+require("dotenv").config();
 
 var client = new Twitter({
-  consumer_key: "",
-  consumer_secret: "",
-  bearer_token: "",
+  consumer_key: process.env.APIKEY,
+  consumer_secret: process.env.APIKEY,
+  bearer_token: process.env.APIKEY,
 });
 
 var params = { screen_name: "dawa3_4" };
-client.get("useres/lookup", params, function (error, tweets, response) {
+client.get("friends/ids", params, function (error, tweets, response) {
   if (!error) {
     console.log(tweets);
   }
+  console.log("Status code!!!!!");
+  console.log(response.statusCode);
+  console.log(response.toJSON().body);
 });
